5c7302e6b2febf4af67bbc7e74d946f60161f441
Marian <marianbuzak@gmail.com>
2022-10-17 17:02:51 +0200

    Changed database to MySQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -75,13 +75,6 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-DATABASES = {
-    'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'PZ-2',
         'USER': 'root',
