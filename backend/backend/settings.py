3ed2810c13c076dfaa12fffc9ec4673a2f7392f6
Marian <marianbuzak@gmail.com>
2022-10-17 17:01:07 +0200

    Changed database to MySQL

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,6 +80,17 @@ DATABASES = {
     }
 }
 
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'PZ-2',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '3306'
+    }
+}
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
