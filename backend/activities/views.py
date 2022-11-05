f201e277f3cca819cd0ef6db9feac753b3b6cfb6
Marian <marianbuzak@gmail.com>
2022-11-05 20:32:24 +0100

    activity app features

--- backend/activities/views.py
+++ backend/activities/views.py
@@ -1,3 +1,51 @@
-from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 
-# Create your views here.
+from django.views.generic import CreateView, UpdateView, DeleteView, DetailView, View
+from django.views.generic.list import ListView
+
+from .models import Activity, Participant
+from accounts import Account
+
+from django.urls import reverse_lazy
+
+
+class IndexActivity(ListView):
+	# template_name = ''
+	queryset = Activity.objects.all().order_by('datetime')
+
+
+class CreateActivity(LoginRequiredMixin, PermissionRequiredMixin, CreateView):
+	model = Activity
+	fields = '__all__'
+
+	# template_name = ''
+	# success_url = reverse_lazy('')
+	# permission_required = ('',)
+
+	# user which creates new activity must be a lecturer
+	def form_valid(self, form):
+		form.instance.account = Account.objects.get(pk=self.request.user.id)
+		return super(CreateActivity, self).form_valid(form)
+
+
+class UpdateActivity(LoginRequiredMixin, PermissionRequiredMixin, UpdateView):
+	model = Activity
+	fields = '__all__'
+
+	# template_name = ''
+	# permission_required = ('',)
+	# success_url = ''
+
+
+class DeleteActivity(LoginRequiredMixin, PermissionRequiredMixin, DeleteView):
+	model = Activity
+	# success_url = reverse_lazy('')
+	# permission_required = ('',)
+
+
+class CreateParticipant(LoginRequiredMixin, CreateView):
+	model = Participant
+	fields = '__all__'
+
+	# template_name = ''
+	# success_url = reverse_lazy('')
