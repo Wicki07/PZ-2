748d7a1a25e7dba7943f6b9fa84b7208bab46d3a
Marian <marianbuzak@gmail.com>
2023-01-05 13:54:02 +0100

    restore admin panel

--- backend/activities/admin.py
+++ backend/activities/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Activity, Praticipation
 
-# Register your models here.
+
+class ActivityAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Info', {'fields': ('name',)}),
+        ('Date', {'fields': ('date', 'start_time', 'end_time')}),
+        ('Leader', {'fields': ('leader',)}),
+        ('Details', {'fields': ('base_activity', 'business')}),
+    ]
+
+    list_filter = (
+        ('date', admin.DateFieldListFilter),
+        'leader',
+        'base_activity',
+        'business'
+    )
+
+    list_display = ('name', 'date', 'leader', 'base_activity', 'business')
+
+
+class ParticipationtAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('Info', {'fields': ('user', 'activity', 'presence')}),
+    ]
+
+    list_filter = list_display = ('user', 'activity', 'presence')
+
+
+admin.site.register(Activity, ActivityAdmin)
+admin.site.register(Praticipation, ParticipationtAdmin)
--- backend/authentication/admin.py
+++ backend/authentication/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import CustomUser, Business
 
-# Register your models here.
+
+class CustomUserAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('User', {'fields': ('email', 'username', 'first_name', 'last_name')}),
+        ('Detail', {'fields': ('role', 'phone', 'is_staff', 'is_active')}),
+    ]
+
+    list_filter = list_display = ('email', 'role')
+
+
+class BusinessAdmin(admin.ModelAdmin):
+    fieldsets = [
+        ('User', {'fields': ('user',)}),
+        ('Category', {'fields': ('category',)}),
+    ]
+
+    list_filter = list_display = ('user', 'category')
+
+
+admin.site.register(CustomUser, CustomUserAdmin)
+admin.site.register(Business, BusinessAdmin)
